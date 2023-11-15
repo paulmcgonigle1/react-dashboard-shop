f9286d32370b0d969ced54c0c818fadd68abdc6a
paulmcgonigle <S00210326@atu.ie>
2023-11-15 16:59:13 +0000

    dashboard page and dummy data ready

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 import DashboardStatsGrid from './DashboardStatsGrid'
 import TransactionChart from './TransactionChart'
 import BuyerProfileChart from './BuyerProfileChart'
+import RecentOrders from './RecentOrders'
+import PopularProducts from './PopularProducts'
 export default function Dashboard() {
     return (
         <div className="flex flex-col gap-4">
@@ -10,6 +12,10 @@ export default function Dashboard() {
             <div className="flex flex-row gap-4 w-full">
                 <TransactionChart />
                 <BuyerProfileChart />
+            </div>
+            <div className="flex flex-row gap-4 w-full">
+                <RecentOrders />
+                <PopularProducts />
             </div>
         </div>
     )
